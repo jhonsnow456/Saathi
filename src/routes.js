0fdbb4c25cba9d3ff911526c800c4c069c1b694b
The-Anton <shekhar59324@gmail.com>
2022-08-23 16:39:14 +0530

    normal test starter || upload file || speech to text

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ import { Navigate, useRoutes, useNavigate } from "react-router-dom";
 import DashboardLayout from "./layouts/dashboard";
 import LogoOnlyLayout from "./layouts/LogoOnlyLayout";
 //
+import Test from "./pages/test/Test";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import DashboardApp from "./pages/DashboardApp";
+import TestOnboarding from "./pages/test/TestOnboarding.js";
 import NotFound from './pages/Page404';
+import STT from "./components/test/speech-to-text.js";
+import Upload from "./utils/azurefileupload/App.js";
+
 import Profile from "./pages/profile/Profile";
 import { useEffect, useState } from "react";
 import swal from 'sweetalert';
@@ -26,7 +31,9 @@ export default function Router(props) {
           path: "user/:id",
           element: <Profile /> ,
         },
-        { path: "test/new", element: <DashboardApp />  },
+        { path: "test/new", element: <Test />  },
+        { path: "speechtotext", element: <STT />  },
+        { path: "upload", element: <Upload />  },
         { path: "report/:id", element: <DashboardApp />  },
         { path: "login", element: <Login /> },
         { path: "register", element: <Register /> },
