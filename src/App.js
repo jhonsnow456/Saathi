4a2bfac2fe5093d52952bb7d8f40e07e4a3611c8
jhonsnow456 <thakuraman22july@gmail.com>
2022-08-23 19:35:10 +0530

    added Colorblind test

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
 //css
 
-// import TEST_LISTENING from './data/TEST_LISTENING';
-// import TestMaker from './components/testmaker/TestMaker';
-// import Listening from './components/test/Listening';
+import TEST_LISTENING from './data/TEST_LISTENING';
+import TestMaker from './components/testmaker/TestMaker';
+import Listening from './components/test/Listening/Listening';
+
+import TEST_ISHIHARA from './data/TEST_ISHIHARA';
+import Colorblind from './components/test/Colorblind/Colorblind';
 
 import './index.css';
 import { useState } from 'react';
@@ -20,12 +23,14 @@ import { useState } from 'react';
 export default function App() {
   const [email,setEmail]=useState('');
   return (
-    <ThemeConfig>
-      <ScrollToTop />
-      <GlobalStyles />
-      <BaseOptionChartStyle />
-      <Router email={email} setEmail={setEmail}/>
-    </ThemeConfig>
+    // <ThemeConfig>
+    //   <ScrollToTop />
+    //   <GlobalStyles />
+    //   <BaseOptionChartStyle />
+    //   <Router email={email} setEmail={setEmail}/>
+    // </ThemeConfig>
+
     // <TestMaker details={TEST_LISTENING.details} questions={TEST_LISTENING.questions.easy} testComponent={Listening}/>
+    <TestMaker details={TEST_ISHIHARA.details}  questions={TEST_ISHIHARA.questions} testComponent={Colorblind}/>
   );
 }
