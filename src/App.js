0e8b2aa98f143d82e05ade3f713c023ccbe30479
jhonsnow456 <thakuraman22july@gmail.com>
2022-08-24 12:12:36 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
 //css
 
-<<<<<<< HEAD
-import TestMaker from './components/testmaker/TestMaker';
-
-import TEST_LISTENING from './data/TEST_LISTENING';
-import Listening from './components/test/Listening/Listening';
-
-import TEST_ISHIHARA from './data/TEST_ISHIHARA';
-import Colorblind from './components/test/Colorblind/Colorblind';
-
-import TEST_HANDWRITING from './data/TEST_HANDWRITING';
-import Handwriting from './components/test/Handwriting/Handwriting';
-=======
->>>>>>> 5f33da5f919ff817a970402dd44c795e4a85b8ab
 
 import './index.css';
 import { useState } from 'react';
@@ -31,19 +18,6 @@ import { useState } from 'react';
 export default function App() {
   const [email,setEmail]=useState('');
   return (
-<<<<<<< HEAD
-    // <ThemeConfig>
-    //   <ScrollToTop />
-    //   <GlobalStyles />
-    //   <BaseOptionChartStyle />
-    //   <Router email={email} setEmail={setEmail}/>
-    // </ThemeConfig>
-
-    // <TestMaker details={TEST_LISTENING.details} questions={TEST_LISTENING.questions.easy} testComponent={Listening}/>
-    // <TestMaker details={TEST_ISHIHARA.details}  questions={TEST_ISHIHARA.questions} testComponent={Colorblind}/>
-    <TestMaker details={TEST_HANDWRITING.details}  questions={TEST_HANDWRITING.questions} testComponent={Handwriting}/>
-    );
-=======
     <ThemeConfig>
       <ScrollToTop />
       <GlobalStyles />
@@ -51,5 +25,4 @@ export default function App() {
       <Router email={email} setEmail={setEmail}/>
     </ThemeConfig>
   );
->>>>>>> 5f33da5f919ff817a970402dd44c795e4a85b8ab
 }
