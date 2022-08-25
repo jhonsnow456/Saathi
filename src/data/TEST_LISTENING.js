5559811ef668109f9d8504719544731f8ce7052e
ravinder-groww <ravinder.singh@groww.in>
2022-08-26 05:04:11 +0530

    added memory questions with question types & speed

--- src/data/TEST_LISTENING.js
+++ src/data/TEST_LISTENING.js
@@ -7,87 +7,147 @@ const TEST_LISTENING = {
             {
                 title: 'One',
                 answer: 'one',
+                questionType: 'phonological awareness'
             },
             {
                 id: 2,
                 title: 'cat',
                 answer: 'cat',
+                questionType: 'phonological awareness'
             },
             {
                 id: 3,
                 title: 'three',
                 answer: 'three',
+                questionType: 'phonological awareness'
             },
             {
                 id: 4,
                 title: 'bangalore',
                 answer: 'bangalore',
+                questionType: 'phonological awareness'
             },
             {
                 id: 5,
                 title: 'Now say loop but instead of /l/ say /s/',
                 instruction: 'Write final word',
                 answer: 'soop',
+                questionType: 'phonological awareness'
             },
             {
                 id: 6,
                 title: 'Say sleep but don’t say /s/.',
                 instruction: 'Write final word',
                 answer: 'leap',
+                questionType: 'phonological awareness'
             },
             {
                 id: 7,
                 title: 'Say ran. Now say ran but instead of /a/ say /u/.',
                 instruction: 'Write final word',
                 answer: 'run',
+                questionType: 'phonological awareness'
             },
             {
                 id: 8,
                 title: 'Say bread. Now say bread but don’t say /r/.',
                 instruction: 'Write final word',
                 answer: 'bed',
+                questionType: 'phonological awareness'
             },
             {
                 id: 9,
                 title: 'Say some. Say some but instead of /m/ say /n/.',
                 instruction: 'Write final word',
                 answer: 'sun',
+                questionType: 'phonological awareness'
             },
             {
                 id: 10,
                 title: 'Say ghost. Now say ghost but don’t say /s/.',
                 instruction: 'Write final word',
                 answer: 'goat',
+                questionType: 'phonological awareness'
             },
             {
                 id: 11,
                 title: 'Say leapfrog. Now say leapfrog but don’t say leap.',
                 instruction: 'Write final word',
                 answer: 'frog',
+                questionType: 'phonological awareness'
             },
             {
                 id: 12,
                 title: 'Say carnation. Now say carnation but don’t say car.',
                 instruction: 'Write final word',
                 answer: 'nation',
+                questionType: 'phonological awareness'
             },
             {
                 id: 13,
                 title: 'Say invention. Now say invention but don’t say in.',
                 instruction: 'Write final word',
                 answer: 'vention',
+                questionType: 'phonological awareness'
             },
             {
                 id: 14,
-                title: 'Say sheet. Now say sheet but don’t say /sh/.',
+                title: 'Say sheet. Now say sheet but don’t say she.',
                 instruction: 'Write final word',
                 answer: 'eat',
+                questionType: 'phonological awareness'
             },
             {
                 id: 15,
                 title: 'Say tornado. Now say tornado but don’t say tor.',
                 instruction: 'Write final word',
                 answer: 'nado',
+                questionType: 'phonological awareness'
+            },
+            //memory tests
+            {
+                id: 16,
+                title: 'U M',
+                instruction: 'Write the sequence of words',
+                answer: 'UM',
+                questionType: 'memory'
+            },
+            {
+                id: 17,
+                title: 'T Z L D',
+                instruction: 'Write the sequence of words',
+                answer: 'TZLD',
+                questionType: 'memory'
+            },
+            {
+                id: 18,
+                title: 'K X C E J O',
+                instruction: 'Write the sequence of words',
+                answer: 'KXCEJO',
+                questionType: 'memory'
+            },
+            {
+                id: 19,
+                title: 'A V C Y I S E H',
+                instruction: 'Write the sequence of words',
+                answer: 'AVCYISEH',
+                questionType: 'memory'
+            },
+            {
+                id: 20,
+                title: 'L B F Q R P M A U X',
+                instruction: 'Write the sequence of words',
+                answer: 'LBFQRPMAUX',
+                speed: 0.5,
+                questionType: 'memory'
+            },
+            {
+                id: 21,
+                title: 'Z Q E C T B U M O N R V',
+                instruction: 'Write the sequence of words',
+                answer: 'ZQECTBUMONRV',
+                speed: 0.1,
+                questionType: 'memory'
             }
         ]
     }
