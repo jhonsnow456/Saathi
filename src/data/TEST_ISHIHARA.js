ec8cab3a3fe0c6d64bdf9fd9f2718396656aa7c0
jhonsnow456 <thakuraman22july@gmail.com>
2022-08-25 14:10:20 +0530

    added the updated ISHIHARA test

--- src/data/TEST_ISHIHARA.js
+++ src/data/TEST_ISHIHARA.js
@@ -3,67 +3,307 @@ const TEST_ISHIHARA = {
         name: 'Ishihara'
     },
     questions: [
-    {
-        "id": 1,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image1.jpg",
-        "value": 7
-    },
-    {
-        "id": 2,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image2.jpg",
-        "value": 6
-    },
-    {
-        "id": 3,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image3.jpg",
-        "value": 26
-    },
-    {
-        "id": 4,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image4.jpg",
-        "value": 15
-    },
-    {
-        "id": 5,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image5.jpg",
-        "value": 6
-    },
-    {
-        "id": 6,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image6.jpg",
-        "value": 73
-    },
-    {
-        "id": 7,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image7.jpg",
-        "value": 5
-    },
-    {
-        "id": 8,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image8.jpg",
-        "value": 16
-    },
-    {
-        "id": 9,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image9.jpg",
-        "value": 45
-    },
-    {
-        "id": 10,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image10.jpg",
-        "value": 12
-    },
-    {
-        "id": 11,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image11.jpg",
-        "value": 29
-    },
-    {
-        "id": 12,
-        "image": "https://cdn-beaai.nitrocdn.com/DsHNrqyidSdrnEUwxpnDFmLjguAlTfrt/assets/static/optimized/rev-6ede763/wp-content/uploads/2015/08/colorblind-test-image12.jpg",
-        "value": 8
-    }
-]
+        {
+            "id": 1,
+            "image": require('../assets/colorblindness/plate-1.png').default,
+            "vision": {
+                "normalView": 12,
+                "colorBlind":{
+                    "RG": 12,
+                    "protonopia": 12,
+                    "deutonopia": 12
+                }
+            }
+        },
+        {
+            "id": 2,
+            "image": require('../assets/colorblindness/plate-2.png').default,
+            "vision": {
+                "normalView": 8,
+                "colorBlind":{
+                    "RG": 3,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 3,
+            "image": require('../assets/colorblindness/plate-3.png').default,
+            "vision": {
+                "normalView": 6,
+                "colorBlind":{
+                    "RG": 5,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 4,
+            "image": require('../assets/colorblindness/plate-4.png').default,
+            "vision": {
+                "normalView": 29,
+                "colorBlind":{
+                    "RG": 70,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 5,
+            "image": require('../assets/colorblindness/plate-5.png').default,
+            "vision": {
+                "normalView": 57,
+                "colorBlind":{
+                    "RG": 35,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 6,
+            "image": require('../assets/colorblindness/plate-6.png').default,
+            "vision": {
+                "normalView": 5,
+                "colorBlind":{
+                    "RG": 2,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 7,
+            "image": require('../assets/colorblindness/plate-7.png').default,
+            "vision": {
+                "normalView": 3,
+                "colorBlind":{
+                    "RG": 5,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 8,
+            "image": require('../assets/colorblindness/plate-8.png').default,
+            "vision": {
+                "normalView": 15,
+                "colorBlind":{
+                    "RG": 17,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 9,
+            "image": require('../assets/colorblindness/plate-9.png').default,
+            "vision": {
+                "normalView": 74,
+                "colorBlind":{
+                    "RG": 21,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 10,
+            "image": require('../assets/colorblindness/plate-10.png').default,
+            "vision": {
+                "normalView": 2,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 11,
+            "image": require('../assets/colorblindness/plate-11.png').default,
+            "vision": {
+                "normalView": 6,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 12,
+            "image": require('../assets/colorblindness/plate-12.png').default,
+            "vision": {
+                "normalView": 97,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 13,
+            "image": require('../assets/colorblindness/plate-13.png').default,
+            "vision": {
+                "normalView": 45,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 14,
+            "image": require('../assets/colorblindness/plate-14.png').default,
+            "vision": {
+                "normalView": 5,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 15,
+            "image": require('../assets/colorblindness/plate-15.png').default,
+            "vision": {
+                "normalView": 7,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 16,
+            "image": require('../assets/colorblindness/plate-16.png').default,
+            "vision": {
+                "normalView": 16,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 17,
+            "image": require('../assets/colorblindness/plate-17.png').default,
+            "vision": {
+                "normalView": 73,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 18,
+            "image": require('../assets/colorblindness/plate-18.png').default,
+            "vision": {
+                "normalView": null,
+                "colorBlind":{
+                    "RG": 5,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 19,
+            "image": require('../assets/colorblindness/plate-19.png').default,
+            "vision": {
+                "normalView": null,
+                "colorBlind":{
+                    "RG": 2,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 20,
+            "image": require('../assets/colorblindness/plate-20.png').default,
+            "vision": {
+                "normalView": null,
+                "colorBlind":{
+                    "RG": 45,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 21,
+            "image": require('../assets/colorblindness/plate-21.png').default,
+            "vision": {
+                "normalView": null,
+                "colorBlind":{
+                    "RG": 73,
+                    "protonopia": null,
+                    "deutonopia": null
+                }
+            }
+        },
+        {
+            "id": 22,
+            "image": require('../assets/colorblindness/plate-22.png').default,
+            "vision": {
+                "normalView": 26,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": 6,
+                    "deutonopia": 2
+                }
+            }
+        },
+        {
+            "id": 23,
+            "image": require('../assets/colorblindness/plate-23.png').default,
+            "vision": {
+                "normalView": 42,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": 2,
+                    "deutonopia": 4
+                }
+            }
+        },
+        {
+            "id": 24,
+            "image": require('../assets/colorblindness/plate-24.png').default,
+            "vision": {
+                "normalView": 35,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": 5,
+                    "deutonopia": 3
+                }
+            }
+        },
+        {
+            "id": 25,
+            "image": require('../assets/colorblindness/plate-25.png').default,
+            "vision": {
+                "normalView": 96,
+                "colorBlind":{
+                    "RG": null,
+                    "protonopia": 6,
+                    "deutonopia": 9
+                }
+            }
+        },
+    ]
 }
 
 export default TEST_ISHIHARA
