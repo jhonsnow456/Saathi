44ddfc6174f01d47b6b047f3faedf9fbc318c8d5
jhonsnow456 <thakuraman22july@gmail.com>
2022-08-26 12:37:34 +0530

    added feature of file upload

---
+++ src/data/TEST_UPLOADS.js
@@ -0,0 +1,24 @@
+const TEST_HANDWRITING = {
+    details: {
+        name: 'Historical Uploads',
+    },
+    questions: [
+    {
+        "id": 1,
+        "question": "Upload the your Handwriting Image", 
+        "instruction": "Please upload the pre recorded file"
+    },
+    {
+        "id": 2,
+        "question": "Upload the your recorded Face Video",
+        "instruction": "Please upload the pre recorded file"
+    },
+    {
+        "id": 3,
+        "question": "Upload a pre recorded Audio File",
+        "instruction": "Please upload the pre recorded file"
+    }
+]
+}
+
+export default TEST_HANDWRITING
